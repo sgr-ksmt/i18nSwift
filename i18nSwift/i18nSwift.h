2e81647cf861ccae70cf15a95de5ddae3922a9d2
sgr-ksmt <melodydance.k.s@gmail.com>
2017-03-26 14:07:21 +0900

    setup

---
+++ i18nSwift/i18nSwift.h
@@ -0,0 +1,19 @@
+//
+//  i18nSwift.h
+//  i18nSwift
+//
+//  Created by Suguru Kishimoto on 3/26/17.
+//  Copyright © 2017 Suguru Kishimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for i18nSwift.
+FOUNDATION_EXPORT double i18nSwiftVersionNumber;
+
+//! Project version string for i18nSwift.
+FOUNDATION_EXPORT const unsigned char i18nSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <i18nSwift/PublicHeader.h>
+
+
