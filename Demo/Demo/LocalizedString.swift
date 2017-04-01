1f14d25930dc2d9ffc8e4863fa1f912e5fb86b7a
sgr-ksmt <melodydance.k.s@gmail.com>
2017-04-01 13:48:13 +0900

    setup Demo

---
+++ Demo/Demo/LocalizedString.swift
@@ -0,0 +1,20 @@
+//
+//  LocalizedString.swift
+//  Demo
+//
+//  Created by Suguru Kishimoto on 4/1/17.
+//  Copyright © 2017 Suguru Kishimoto. All rights reserved.
+//
+
+import Foundation
+import i18nSwift
+
+extension LocalizedString {
+    static let alertButton: LocalizedString = "alert.button"
+    static let alertTitle: LocalizedString = "alert.title"
+    static let alertMessage: LocalizedString = "alert.message"
+    static let chooseLanguage: LocalizedString = "choose_language"
+    static let ok: LocalizedString = "ok"
+    static let cancel: LocalizedString = "cancel"
+    static let useSystem: LocalizedString = "use_system_language"
+}
--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
+import i18nSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var alertButton: UIButton! {
+        didSet {
+            alertButton.addTarget(self, action: #selector(alertButtonDidTap(_:)), for: .touchUpInside)
+        }
+    }
+    @IBOutlet private weak var selectLanguageButton: UIButton! {
+        didSet {
+            selectLanguageButton.addTarget(self, action: #selector(selectLanguageButtonDidTap(_:)), for: .touchUpInside)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        update()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    
+    func update() {
+        titleLabel.text = Language.displayName()
+        alertButton.setTitle(I18n.t(.alertButton), for: [])
+        selectLanguageButton.setTitle(I18n.t(.chooseLanguage), for: [])
+    }
+    
+    @objc private func alertButtonDidTap(_: UIButton) {
+        let alert = UIAlertController(title: I18n.t(.alertTitle), message: I18n.t(.alertMessage), preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: I18n.t(.ok) , style: .default) { _ in })
+        show(alert, sender: nil)
+    }
+    
+    @objc private func selectLanguageButtonDidTap(_: UIButton) {
+        let actionSheet = UIAlertController(title: nil, message: I18n.t(.chooseLanguage), preferredStyle: .actionSheet)
+        
+        Language.availableLanguages(includeBase: false).forEach { language in
+            let title = Language.displayName(for: language).map { Language.current == language ? "\($0) ✔︎" : $0 } ?? ""
+            actionSheet.addAction(UIAlertAction(title: title, style: .default) { _ in
+                Language.current = language
+            })
+        }
+        
+        actionSheet.addAction(UIAlertAction(title: I18n.t(.useSystem), style: .destructive) { _ in
+            Language.reset()
+        })
+        actionSheet.addAction(UIAlertAction(title: I18n.t(.cancel), style: .cancel) { _ in })
+        show(actionSheet, sender: nil)
+    }
 }
 
